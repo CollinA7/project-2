22453789deb9233a23b2128861690ff7d4ef3e29
cayean <collinayala@gmail.com>
2022-10-19 21:26:36 -0500

    refactored connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,12 @@
 const Sequelize = require('sequelize');
 require("dotenv").config();
 
-if (process.env.JAWSDB_URL) {
-    const sequelize = new Sequelize(process.env.JAWSDB_URL)
-} else {
-    const sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host: "localhost",
-            dialect: "mysql",
-            port: 3306
-        });
-}
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
 
 module.exports = sequelize;
