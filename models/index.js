000aa6213b96fdb0e92c0c772b0e78be06697f64
Racquel Munoz <racquelg85@hotmail.com>
2022-10-21 15:03:19 -0500

    added the model index file

--- models/index.js
+++ models/index.js
@@ -3,11 +3,25 @@ const Order = require('./Order')
 const User = require('./User')
 
 User.hasMany(Customer, {
-    foreignKey: 'user_username',
+    foreignKey: 'user_id',
+})
+
+Customer.belongsTo(User, {
+    foreignKey: 'user_id',
+})
+
+User.belongsToMany(Customer, {
+    through: Order,
+    as: 'order_post',
+    foreignKey: 'user_id',
+})
+
+Order.belongsTo(User, {
+    foreignKey: 'user_id',
 })
 
 User.hasMany(Order, {
-    foreignKey: 'user_username',
+    foreignKey: 'user_id',
 })
 
 module.exports = { User, Order, Customer }
