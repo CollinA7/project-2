21c0bdccb01985aa0b5c22b5ec19d9a622c20742
TomLuna <lomtuna15@gmail.com>
2022-10-22 10:57:21 -0500

    updated router file

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,6 +4,7 @@ const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
-    res.render('login', {
+  res.render('login', {
+  });
 });
 module.exports = router;
