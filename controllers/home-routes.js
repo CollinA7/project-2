f5c929ffaf2f4a5966177eba0b74dd8fdb82c3c0
cayean <collinayala@gmail.com>
2022-10-22 10:12:12 -0500

    refactored homepage to route to login

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,10 @@ const sequelize = require('sequelize');
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', (req, res) => {
+router.get('/', withAuth, (req, res) => {
     res.render('login', {
+
+    });
 });
+
 module.exports = router;
