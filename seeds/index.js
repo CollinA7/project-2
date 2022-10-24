6b94b0591ee2804debb69a3e0b0c3c775b850153
Racquel Munoz <racquelg85@hotmail.com>
2022-10-24 18:10:03 -0500

    changed seed data

--- seeds/index.js
+++ seeds/index.js
@@ -1,21 +1,20 @@
 // users orders customers
-const seedUsers = require('./user-seeds');
-const seedOrders = require('./orders-seeds');
-const seedCustomers = require('./customer-seeds');
-
-const sequelize = require('../config/connection');
+const sequelize = require('../config/connection')
+const seedOrders = require('./order-seeds')
+const seedCustomers = require('./customer-seeds')
+const seedUsers = require('./user-seeds')
 
 const seedAll = async () => {
-  await sequelize.sync({force: true});
-  console.log('\n----- DATABASE SYNCED -----\n');
-  await seedUsers();
-  console.log('\n----- USERS SEEDED -----\n');
-  await seedOrders();
-  console.log('\n----- ORDERS SEEDED -----\n');
-  await seedCustomers();
-  console.log('\n----- CUSTOMERS SEEDED -----\n');
+    await sequelize.sync({ force: true })
+    console.log('\n----- DATABASE SYNCED -----\n')
+    await seedOrders()
+    console.log('\n----- ORDERS SEEDED -----\n')
+    await seedCustomers()
+    console.log('\n----- CUSTOMERS SEEDED -----\n')
+    await seedUsers()
+    console.log('\n----- USERS SEEDED -----\n')
 
-  process.exit(0);
-};
+    process.exit(0)
+}
 
-seedAll();
+seedAll()
--- seeds/order-seeds.js
+++ seeds/order-seeds.js
@@ -1,26 +1,20 @@
-const { Order } = require('../models');
+const { Order } = require('../models')
 
 const orderData = [
     {
         order_id: 1,
         name: 'Cheeseburger Combo',
-        customer_id: 2,
-        user_id: 2
     },
     {
         order_id: 2,
         name: 'Chicken Strips Combo',
-        customer_id: 3,
-        user_id: 1
     },
     {
         order_id: 3,
         name: 'Kids Meal',
-        customer_id: 4,
-        user_id: 4
-    }
+    },
 ]
 
-const seedOrder = () => Order.bulkCreate(orderData);
+const seedOrder = () => Order.bulkCreate(orderData)
 
-module.exports = seedOrder;
+module.exports = seedOrder
