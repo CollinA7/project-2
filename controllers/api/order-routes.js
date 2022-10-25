844b7746fb1f1a849aae5de8f9481be66d370dde
cayean <collinayala@gmail.com>
2022-10-24 20:36:57 -0500

    fixed redirect bug by removing withAuth

--- controllers/api/order-routes.js
+++ controllers/api/order-routes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router()
 const { Order } = require('../../models')
 const withAuth = require('../../utils/helpers')
-const { Order } = require('../../models')
 
 router.get('/', (req, res) => {
     Order.findAll()
--- public/js/login.js
+++ public/js/login.js
@@ -24,7 +24,7 @@ async function signupFormHandler(event) {
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
     if (username && email && password) {
-        const response = await fetch('/api/users', {
+        const response = await fetch('/api/users/login', {
         method: 'post',
         body: JSON.stringify({
             username,
