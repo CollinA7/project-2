ef61b309686edd706a6e3748a7fba9be2000a51d
cayean <collinayala@gmail.com>
2022-10-25 21:56:19 -0500

    final login functionality for merging

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,5 @@
-/* eslint-disable no-unused-vars */
-// eslint-disable-next-line new-cap
 const router = require('express').Router();
 const sequelize = require('sequelize');
-// eslint-disable-next-line object-curly-spacing
 const { User } = require('../models');
 const withAuth = require('../utils/helpers');
 
